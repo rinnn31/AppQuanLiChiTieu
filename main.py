156376683a6105a94187159b1d8ab6916cd13127
rinnn31 <damquangphongthdk@gmail.com>
2025-10-02 17:25:25 +0700

    forth_commit

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from app.ui.home import HomeWindow
 from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import Qt
 
 if __name__ == "__main__":
     app = QApplication([])
+    app.setAttribute(Qt.ApplicationAttribute.AA_EnableHighDpiScaling, True)
+    app.setAttribute(Qt.ApplicationAttribute.AA_UseHighDpiPixmaps, True)
     window = HomeWindow()
     window.show()
     app.exec()
